dbb5eef9ba162b0a19ed28e94553e861a6c6d443
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-03-13 23:53:58 +0100

    Form to input answer was made

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 $(function () {
+
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+        var userAnswer = $("#user_answer").val();
+        console.log(userAnswer);
+    });
+
     function generateRandomNum() {
         var randomNum = "";
         for(var i = 0; i < 4; i++){
             randomNum += Math.floor(Math.random() * 10);
         }
     }
+
+
     generateRandomNum();
 });
