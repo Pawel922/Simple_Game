0aa6fe90ffa683645e0cf3a02dd00b9faf838ae3
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-03-16 22:13:15 +0100

    User answer is checked and colors of input elements are changed properly

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 $(function () {
 
-
+    var randomNum = generateRandomNum();
+    console.log(randomNum);
 
     $("form").on("submit", function (e) {
         e.preventDefault();
-        var userAnswer = [];
         var inputs = $(".user_answer");
-        for(var i = 0; i < inputs.length; i++){
-            userAnswer.push(inputs[i].value);
+        for(var i = 0; i < inputs.length; i++) {
+            var numValueOfInput = parseInt(inputs[i].value);
+            if(numValueOfInput === randomNum[i]){
+                inputs[i].style.background = "green";
+            } else {
+                inputs[i].style.background = "red";
+            }
         }
-        console.log(userAnswer);
     });
 
     function generateRandomNum() {
         var randomNum = [];
-        for(var i = 0; i < 4; i++){
+        for(var i = 0; i < 4; i++) {
             randomNum.push(Math.floor(Math.random() * 10));
         }
         return randomNum;
     }
 
-    console.log(generateRandomNum())
 });
